f67c41c27e9570f36c9a9c42301e1bb04b5b8c3c
Mohammad Hossein <136825585+mohjafari23@users.noreply.github.com>
2023-08-03 16:35:13 +0330

    Create weightDM.m

---
+++ weightDM.m
@@ -0,0 +1,6 @@
+function wdm = weightDM (FDMn)
+ % based on the Fermatean Number of the DM. The weight of each DM is calculated.
+FDMn=FDMn.^3;
+pi=1-FDMn(1,:)-FDMn(2,:);
+wdm=(FDMn(1,:)+pi.*(FDMn(1,:)./sum(FDMn)))/sum(FDMn(1,:)+pi.*(FDMn(1,:)./sum(FDMn)),2);
+end
