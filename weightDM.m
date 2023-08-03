4ac9f6ddac32371b29be927d7ad3d4a3372aa9fa
Mohammad Hossein <136825585+mohjafari23@users.noreply.github.com>
2023-08-03 16:39:42 +0330

    Update weightDM.m

--- weightDM.m
+++ weightDM.m
@@ -3,4 +3,5 @@ function wdm = weightDM (FDMn)
 FDMn=FDMn.^3;
 pi=1-FDMn(1,:)-FDMn(2,:);
 wdm=(FDMn(1,:)+pi.*(FDMn(1,:)./sum(FDMn)))/sum(FDMn(1,:)+pi.*(FDMn(1,:)./sum(FDMn)),2);
+% wdm is a row vector with the number of DMs
 end
