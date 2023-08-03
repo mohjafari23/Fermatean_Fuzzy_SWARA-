4c0a035036c2c1803158f32c6b67abada6dc5b64
Mohammad Hossein <136825585+mohjafari23@users.noreply.github.com>
2023-08-03 16:37:59 +0330

    Update getDMmembership.m

--- getDMmembership.m
+++ getDMmembership.m
@@ -5,8 +5,8 @@ function FDMn=getDMmembership(EY)
       EY=EY';
   end
 % The membership is calculated by the sigmoidal membership function
-    MD = sigmf(EY,[3,1.7]);
-    NMD = sigmf(EY,[-3,1.3]);
+    MD = sigmf(EY,[3,1.7]); %is the membership grade
+    NMD = sigmf(EY,[-3,1.3]); %is none membership grade
     FDMn=[MD;NMD];
-
+ % the FDMn is a matrix with 2 rows (first for membership grade and the second for the none membership grade) and the number of DM columns.
 end
