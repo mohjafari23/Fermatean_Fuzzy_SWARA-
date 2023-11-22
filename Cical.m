c86a75242a8557f8a95d777e4a4c49a0a0f03b26
Mohammad Hossein <136825585+mohjafari23@users.noreply.github.com>
2023-11-22 23:19:09 +0330

    Update Cical.m

--- Cical.m
+++ Cical.m
@@ -3,7 +3,7 @@ function Ci = Cical(fscore)
   [nc,~]=size(fscore);
   Ci=zeros([nc,2]);
   [sfscore,Ci(:,1)] = sort(fscore);
-  Ci(:,2)=sfscore-[sfscore(1);sfscore(1:end-1)];
+  Ci(:,2)=abs(sfscore-[sfscore(1);sfscore(1:end-1)]);
 % Ci is a matrix with size number of criteria and 2 columns the first is
 % the index of criteria and the second is the Ci
 end
