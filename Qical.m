c6027c3106eccb1d16c79554d941bb4ea7ca7e29
Mohammad Hossein <136825585+mohjafari23@users.noreply.github.com>
2023-11-22 23:19:56 +0330

    Update Qical.m

--- Qical.m
+++ Qical.m
@@ -5,7 +5,7 @@ function Qi = Qical (Ki)
   Qi(:,1) = Ki(:,1);
   Qi(1,2) = 1;
    for i = 2 : nc
-      Qi(i,2) = Qi(i-1,2)/Ki(i);
+      Qi(i,2) = Qi(i-1,2)/Ki(i,2);
    end
  % Qi is a matrix with a size number of criteria and 2 columns the first is
 % the index of criteria and the second is the Qi
